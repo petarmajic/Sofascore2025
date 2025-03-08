1e6f655d446811d6e7a90ff389f7ff289d5e906e
Petar Majić <petar.majic@fer.unizg.hr>
2025-03-08 20:54:24 +0100

    Xcode project init commit

---
+++ Sofascore2025App/Sofascore2025App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Sofascore2025App
+//
+//  Created by Petar Majić on 08.03.2025..
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
